536387ed4a4256df4880bb4d09ce2e02f19cfd2a
HAYBISH10 <hassanissackmohamed172@gmail.com>
2024-05-05 13:48:02 +0300

    example2 switch lab

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-//Mon - 7am
-//Tue - Thurs - 4am
-//Fri - 9am
-//Sat - Sun - 8am
+// //Mon - 7am
+// //Tue - Thurs - 4am
+// //Fri - 9am
+// //Sat - Sun - 8am
 
-let day = 'Monday'
+// let day = 'Monday'
 
-switch(day){
-    case "Monday" :
-        console.log("7am")
-        break;
-        case "Tuesday" :
-        console.log("4am")
-        case "Wedday" :
-        console.log("4am")
-        case "Thursday" :
-        console.log("4am")
-        case "Friday" :
-        console.log("9am")
+// switch(day){
+//     case "Monday" :
+//         console.log("7am")
+//         break;
+//         case "Tuesday" :
+//         case "Wedday" :
+//         case "Thursday" :
+//             console.log("4am")
+//             break;
+//         case "Friday" :
+//         console.log("9am")
+
+//         default:
+
+// }
+
+let month = 'january'
+switch (month){
+    case "january":
+        console.log('30days')
+    case "febrary":
+        console.log('28/29')
+    case "march":
+        console.log('30day')
 }
     
