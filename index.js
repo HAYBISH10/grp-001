2fa63fcd807b8e3e989725775c5c2476b44c25de
HAYBISH10 <hassanissackmohamed172@gmail.com>
2024-05-05 13:01:55 +0300

    group1 switch lab

---
+++ index.js
@@ -0,0 +1,21 @@
+//Mon - 7am
+//Tue - Thurs - 4am
+//Fri - 9am
+//Sat - Sun - 8am
+
+let day = 'Monday'
+
+switch(day){
+    case "Monday" :
+        console.log("7am")
+        break;
+        case "Tuesday" :
+        console.log("4am")
+        case "Wedday" :
+        console.log("4am")
+        case "Thursday" :
+        console.log("4am")
+        case "Friday" :
+        console.log("9am")
+}
+    
